5f68e3a12b26aba87cebff7d5f52939a1872cc5e
Yuri Martinovsky <vvrite@me.com>
2021-08-04 15:42:24 +0300

    Initial Commit

---
+++ HWStateAndDataFlow/HWStateAndDataFlowApp.swift
@@ -0,0 +1,17 @@
+//
+//  HWStateAndDataFlowApp.swift
+//  HWStateAndDataFlow
+//
+//  Created by Yuriy Martinovskiy on 04.08.2021.
+//
+
+import SwiftUI
+
+@main
+struct HWStateAndDataFlowApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
